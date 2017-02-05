fa04f194750fb6edbeb4a214a4079530a1f73909
Stubenhocker1399 <stubenhocker1399@gmail.com>
2017-02-05 12:19:25 +0100

    Initial commit with console example.

---
+++ NBTReaderConsole/BigEndianHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace NBTReaderConsole
+{
+    public static class BigEndianHelper
+    {
+        private class ValueType
+        {
+            public readonly int Size;
+            public readonly object ConvertAction;
+
+            public ValueType(Type type)
+            {
+                Size = Marshal.SizeOf(type);
+                if (type == typeof(byte))
+                {
+                    ConvertAction = (Func<byte[], int, byte>) ((buffer, index) => buffer[index]);
+                    return;
+                }
+
+                ConvertAction = typeof(BitConverter)
+                    .GetMethod($"To{type.Name}", BindingFlags.Static | BindingFlags.Public)
+                    .CreateDelegate(typeof(Func<,,>).MakeGenericType(typeof(byte[]), typeof(int), type));
+            }
+        }
+
+        private static Dictionary<Type, ValueType> _sValueTypes = new Dictionary<Type, ValueType>();
+        private static ValueType GetValueType<T>()
+        {
+            ValueType valueType;
+            if (_sValueTypes.TryGetValue(typeof(T), out valueType)) return valueType;
+
+            valueType = new ValueType(typeof(T));
+            _sValueTypes.Add(typeof(T), valueType);
+            return valueType;
+        }
+
+        [ThreadStatic]
+        private static byte[] _sReadBuffer;
+        public static T ReadBigEndian<T>(BinaryReader reader)
+            where T : struct
+        {
+            var valueType = GetValueType<T>();
+            if (_sReadBuffer == null) _sReadBuffer = new byte[8];
+
+            reader.Read(_sReadBuffer, 0, valueType.Size);
+
+            var half = valueType.Size >> 1;
+            for (var i = 0; i < half; ++i)
+            {
+                var temp = _sReadBuffer[i];
+                var opposite = valueType.Size - i - 1;
+                _sReadBuffer[i] = _sReadBuffer[opposite];
+                _sReadBuffer[opposite] = temp;
+            }
+
+            return ((Func<byte[], int, T>)valueType.ConvertAction)(_sReadBuffer, 0);
+        }
+    }
+}
---
+++ NBTReaderConsole/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBTReaderConsole
+{
+    class Program
+    {     
+        static void Main(string[] args)
+        {
+            var worldfolder = "C:\\Users\\Chris\\AppData\\LocalLow\\Facepunch Studios\\Chunks\\Worlds\\testworldblocks";
+            var file = "debugDecompressedChunks.0.0";
+            //var file = "level.dat";
+            if (!File.Exists(worldfolder + "\\" + file))
+                Console.WriteLine("Couldn't locate Minecraft World to import.");
+            else
+            {
+                //byte[] leveldatcompressed = File.ReadAllBytes(worldfolder + "\\" + file);
+                //byte[] leveldat = Decompress(leveldatcompressed);
+                byte[] leveldat = File.ReadAllBytes(worldfolder + "\\" + file);
+                Console.WriteLine(BitConverter.ToString(leveldat));
+                Console.WriteLine(Encoding.Default.GetString(leveldat));
+                Console.WriteLine("");
+
+                var nbt = NBTReader.readNBT(leveldat);
+                //var inner = (double) nbt.tree["Data"]["Player"]["Pos"][0];
+                ;
+            }
+            Console.ReadKey();
+        }
+        static byte[] Decompress(byte[] gzip)
+        {
+            // Create a GZIP stream with decompression mode.
+            // ... Then create a buffer and write into while reading from the GZIP stream.
+            using (GZipStream stream = new GZipStream(new MemoryStream(gzip),
+                CompressionMode.Decompress))
+            {
+                const int size = 4096;
+                byte[] buffer = new byte[size];
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = stream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+                    while (count > 0);
+                    return memory.ToArray();
+                }
+            }
+        }
+    }
+}
